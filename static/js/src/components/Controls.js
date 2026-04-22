ed59765cb8fb14f226b3ace3f0a665467815ba7c
Timo <sitizstorm@gmail.com>
2026-04-22 19:40:22 +0200

    fix(ui): intro message + disable buttons during loading

--- static/js/src/components/Controls.js
+++ static/js/src/components/Controls.js
@@ -2,11 +2,12 @@ import React from 'react'
 import IconButton from './IconButton.js'
 
 export default function Controls({ onGenerate, onRate, loading, metrics, userRating }) {
+  const canRate = !loading
   return (
     React.createElement('div', { className: 'actions' },
       React.createElement('button', { onClick: onGenerate, disabled: loading, className: 'primary' }, loading ? 'Génération…' : 'Nouvelle blague'),
-      React.createElement(IconButton, { onClick: () => onRate(1), ariaLabel: "J'aime", iconSrc: '/like.svg', active: userRating === 1 }),
-      React.createElement(IconButton, { onClick: () => onRate(-1), ariaLabel: "Je n'aime pas", iconSrc: '/dislike.svg', active: userRating === -1 })
+      React.createElement(IconButton, { onClick: canRate ? () => onRate(1) : undefined, ariaLabel: "J'aime", iconSrc: '/like.svg', active: userRating === 1, disabled: !canRate }),
+      React.createElement(IconButton, { onClick: canRate ? () => onRate(-1) : undefined, ariaLabel: "Je n'aime pas", iconSrc: '/dislike.svg', active: userRating === -1, disabled: !canRate })
     )
   )
 }
--- static/js/src/components/IconButton.js
+++ static/js/src/components/IconButton.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function IconButton({ onClick, ariaLabel, iconSrc, active }) {
+export default function IconButton({ onClick, ariaLabel, iconSrc, active, disabled }) {
   return (
-    React.createElement('button', { className: 'icon' + (active ? ' active' : ''), onClick, 'aria-label': ariaLabel },
+    React.createElement('button', { className: 'icon' + (active ? ' active' : '') + (disabled ? ' disabled' : ''), onClick: disabled ? undefined : onClick, 'aria-label': ariaLabel, 'aria-disabled': disabled },
       React.createElement('img', { src: iconSrc, width: 20, height: 20, alt: ariaLabel })
     )
   )
